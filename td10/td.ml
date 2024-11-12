02ee6b3b734632224a289619982a58ac661b8dd0
Raghid Osseiran <raghid.osseiran@etu.u-bordeaux.fr>
2024-11-12 14:14:23 +0100

    finished td10

--- td10/td.ml
+++ td10/td.ml
@@ -37,9 +37,18 @@ let maximum l = List.fold_left (fun x y -> if (x > y) then x else y) min_int l;;
 let filter p l = List.fold_left (fun x y -> if (p y) then y::x else x) [] l;;
 
 
-let rec in_list x l =
-  match l with 
-  | [] -> false 
-  | h :: tail -> if (x = h) then true else in_list x tail;;
+let remove_duplicates l = List.fold_left (fun acc x -> filter (fun y -> y <> x) acc @ [x]) [] l;;
 
-let remove_duplicates l = List.fold_left (fun x y -> filter in_list)
+let append l1 l2 = List.fold_right ( fun x acc -> x :: acc) l1 l2;;
+
+
+let map f l = List.rev(List.fold_left (fun acc x -> (f x) :: acc) [] l);;
+
+
+let seq_max_len l e = 
+  let rec aux_max_len l e curr_acc max_acc = 
+    match l with 
+    | [] -> max curr_acc max_acc
+    | h :: tail -> if h = e then aux_max_len tail e (curr_acc + 1) max_acc 
+                   else aux_max_len tail e 0 (max curr_acc max_acc)
+  in aux_max_len l e 0 0;;
